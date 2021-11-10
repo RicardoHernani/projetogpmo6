788cdb1d3328238c519943bd75750aaab6053959
RicardoHernani <ricardohernani@yahoo.com.br>
2021-11-10 10:16:25 -0300

    Projeto criado

---
+++ src/main/java/com/ricardochaves/projetogpmo6/Projetogpmo6Application.java
@@ -0,0 +1,13 @@
+package com.ricardochaves.projetogpmo6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetogpmo6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetogpmo6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardochaves/projetogpmo6/Projetogpmo6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardochaves.projetogpmo6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetogpmo6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
