8db695b4d79f6a12d4bf8f39ffc803be6625dfa2
RicardoHernani <ricardohernani@yahoo.com.br>
2021-11-27 12:50:56 -0300

    Ajustes no profile de teste

--- src/main/java/com/ricardochaves/projetogpmo6/Projetogpmo6Application.java
+++ src/main/java/com/ricardochaves/projetogpmo6/Projetogpmo6Application.java
@@ -1,13 +1,19 @@
 package com.ricardochaves.projetogpmo6;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Projetogpmo6Application {
+public class Projetogpmo6Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projetogpmo6Application.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+
 	}
 
 }
